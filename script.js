bedb660832a6df5e46b6affeceb3e7d4c5b69128
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2024-11-15 18:54:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const byteSize = (str) => {
-  // write your code here
+	let s = new Blob(str);
+	return s.size;
 };
 
-// Do not change the code below
+Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
